8cfa6cc236afe59818a0d2440bd9db68feb62b64
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-01-30 23:17:53 +0000

    Trying to get drone to take off. Not working...

--- js/index.js
+++ js/index.js
@@ -1,69 +1,182 @@
+'use strict';
+
 (function() {
 
-  const SERVICE_UUIDS = [
-    '9a66fa00-0800-9191-11e4-012d1540cb8e',
-    '9a66fb00-0800-9191-11e4-012d1540cb8e',
-    '9a66fc00-0800-9191-11e4-012d1540cb8e',
-    '9a66fd21-0800-9191-11e4-012d1540cb8e',
-    '9a66fd51-0800-9191-11e4-012d1540cb8e',
-    '9a66fe00-0800-9191-11e4-012d1540cb8e'
-  ];
+  const SERVICES = {
+    'fa00': formatUUID('fa00'),
+    'fb00': formatUUID('fb00'),
+    'fc00': formatUUID('fc00'),
+    'fd21': formatUUID('fd21'),
+    'fd51': formatUUID('fd51'),
+    'fe00': formatUUID('fe00')
+  };
 
-  const CHARACTERISTIC_UUIDS = [
-    '9a66fa0b-0800-9191-11e4-012d1540cb8e'
-  ];
+  // There are 32 'Write Without Response' characteristics in the fa00 service! But only using first one.
+  const WRITE_WITHOUT_RESPONSE_CHARACTERISTICS = {
+    'fa0b': formatUUID('fa0b')
+  };
+  
+  // There are 32 'Notify' characteristics in the fb00 service! But only using four of them.
+  const NOTIFY_CHARACTERISTICS = {
+    'fb0f': formatUUID('fb0f'),
+    'fb0e': formatUUID('fb0e'),
+    'fb1b': formatUUID('fb1b'),
+    'fb1c': formatUUID('fb1c')
+  };
 
-  let goButton = document.getElementById('goButton'),
-      droneDevice = null;
+  // There is 1 'Write' characteristic in the fc00 service. Not currently used.
+  const WRITE_CHARACTERISTIC = {
+    'ffc1': formatUUID('ffc1')
+  }
+
+  // There are 3 'Read Write Notify' characteristics in the fd21 service.
+  const READ_WRITE_NOTIFY_CHARACTERISTICS_1 = {
+    'fd22': formatUUID('fd22'),
+    'fd23': formatUUID('fd23'),
+    'fd24': formatUUID('fd24')
+  };
+
+  // There are 3 'Read Write Notify' characteristics in the fd51 service
+  const READ_WRITE_NOTIFY_CHARACTERISTICS_2 = {
+    'fd52': formatUUID('fd52'),
+    'fd53': formatUUID('fd53'),
+    'fd54': formatUUID('fd54')
+  };
+
+  // There are 2 more characteristics in the fe00 service. Not currently used.
+  const MISC_CHARACTERISTICS = {
+    'fe01': formatUUID('fe01'),
+    'fe02': formatUUID('fe02')
+  };
 
 
-  function takeOff() {
+  function formatUUID(uniqueSegment) {
+    return '9a66' + uniqueSegment + '-0800-9191-11e4-012d1540cb8e';
+  }
+
+  function startNotifications(service, characteristicUUID) {
+
+    console.log('Start notifications', characteristicUUID);
+
+    service.getCharacteristic(characteristicUUID)
+      .then((characteristic) => {
+
+        characteristic.startNotifications().then(() => {
+          characteristic.addEventListener('characteristicvaluechanged', event => {
+            console.log('Notification from:', characteristicUUID);
+            console.log('> characteristicvaluechanged = ' + event.target.value + ' [' + event.target.value.byteLength + ']');
+          });
+        });
+
+      });
 
   }
 
+  let goButton = document.getElementById('goButton'),
+      droneDevice = null,
+      gattServer = null;
 
-  goButton.addEventListener('click', () => {
 
-    navigator.bluetooth.requestDevice({
-        filters: [{
-          namePrefix: 'Travis_' // Travis (Airborne Cargo) drone - change to RS_ for Rolling Spider, etc.
-        }]
-      })
-      .then(device => {
+  function discover() {
+    console.log('Searching for drone...');
+    return navigator.bluetooth.requestDevice({
+      filters: [{
+        namePrefix: 'Travis_' // Travis (Airborne Cargo) drone - change to RS_ for Rolling Spider, etc.
+      }]
+    });
+  }
 
-        console.log('Discovered Parrot Mini Drone!', device);
+  function connect(device) {
 
-        droneDevice = device;
+    console.log('Discovered drone', device);
 
-        return device.connectGATT();
+    droneDevice = device;
 
-      })
-      .then(server => {
+    return droneDevice.connectGATT();
 
-        console.log('GATT server', server);
+  }
 
-        return server.getPrimaryService( SERVICE_UUIDS[0] );
+  function registerNotifications(server) {
 
-      })
+    console.log('Register notifications...');
+
+    gattServer = server;
+
+    return gattServer.getPrimaryService( SERVICES.fb00 )
       .then(service => {
 
         console.log('Service', service);
 
-        return service.getCharacteristic( CHARACTERISTIC_UUIDS[0] );
+        ['fb0f', 'fb0e', 'fb1b', 'fb1c'].forEach((key) => {
+          startNotifications(service, NOTIFY_CHARACTERISTICS[key]);
+        });
 
-      })
+        // TODO: 'fd22', 'fd23', 'fd24', 'fd52', 'fd53', 'fd54'
+
+      });
+
+  }
+
+  function handshake() {
+
+    console.log('Handshake...');
+
+    return gattServer.getPrimaryService( SERVICES.fa00 )
+      .then(service => { return service.getCharacteristic( WRITE_WITHOUT_RESPONSE_CHARACTERISTICS.fa0b ) })
       .then(characteristic => {
 
         console.log('Characteristic', characteristic);
 
-        //takeOff();
+        const handshakeCmd = new Uint8Array([0x04, 0x01, 0x00, 0x04, 0x01, 0x00, 0x32, 0x30, 0x31, 0x34, 0x2D, 0x31, 0x30, 0x2D, 0x32, 0x38, 0x00]);
 
-      })
-      .catch(error => {
-        console.error('Error', error);
+        console.log('Writing handshake command');
+
+        return characteristic.writeValue(handshakeCmd);
+
       });
 
+  }
+
+  function takeOff() {
+
+    console.log('Take off...');
+
+    return gattServer.getPrimaryService( SERVICES.fa00 )
+      .then(service => { return service.getCharacteristic( WRITE_WITHOUT_RESPONSE_CHARACTERISTICS.fa0b ) })
+      .then(characteristic => {
+
+        const takeOffCmd = new Uint8Array([0x02, 0x01, 0x00, 0x01, 0x00]);
+
+        console.log('Writing take off command');
+
+        return characteristic.writeValue(takeOffCmd);
+
+      });
+
+  }
+
+  function initialiseAndTakeOff() {
+
+    discover()
+      .then(device => { return connect(device) })
+      .then(server => { return registerNotifications(server) })
+      .then(() => { return handshake() })
+      .then(() => { return takeOff() })
+      .catch(error => { console.error('Error', error) });
+
+  }
+
+  goButton.addEventListener('click', () => {
+    initialiseAndTakeOff();
   });
 
+  /*
+  return new Promise(function(resolve) {
+    console.log('Waiting...');
+    setTimeout(function () {
+      resolve();
+    }, 30000);
+  });
+  */
 
 })();
