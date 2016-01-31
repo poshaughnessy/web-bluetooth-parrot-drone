7b864e36f9af27261e680d03aa49823c3418c65b
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-01-31 01:12:04 +0000

    Takes off but doesn't land - beware

--- js/index.js
+++ js/index.js
@@ -11,8 +11,9 @@
     'fe00': formatUUID('fe00')
   };
 
-  // There are 32 'Write Without Response' characteristics in the fa00 service! But only using first one.
+  // There are 32 'Write Without Response' characteristics in the fa00 service! But only using a couple of them.
   const WRITE_WITHOUT_RESPONSE_CHARACTERISTICS = {
+    'fa0a': formatUUID('fa0a'),
     'fa0b': formatUUID('fa0b')
   };
   
@@ -49,6 +50,27 @@
     'fe02': formatUUID('fe02')
   };
 
+  //const Buffer = window.buffer.Buffer;
+
+  let goButton = document.getElementById('goButton'),
+    droneDevice = null,
+    gattServer = null,
+    ping = null,
+    driveStepsRemaining = 0,
+    // Used to store the 'counter' that's sent to each characteristic
+    steps = {
+      'fa0a': 1,
+      'fa0b': 1,
+      'fa0c': 1
+    },
+    speeds = {
+      yaw: 0, // turn
+      pitch: 0, // forward/backward
+      roll: 0, // left/right
+      altitude: 0 // up/down
+    };
+
+
 
   function formatUUID(uniqueSegment) {
     return '9a66' + uniqueSegment + '-0800-9191-11e4-012d1540cb8e';
@@ -64,7 +86,7 @@
         characteristic.startNotifications().then(() => {
           characteristic.addEventListener('characteristicvaluechanged', event => {
             console.log('Notification from:', characteristicUUID);
-            console.log('> characteristicvaluechanged = ' + event.target.value + ' [' + event.target.value.byteLength + ']');
+            console.log('> characteristicvaluechanged', event.target.value, event.target.value.byteLength);
           });
         });
 
@@ -72,11 +94,6 @@
       .catch(error => { console.error('startNotifications error', error)});
 
   }
-
-  let goButton = document.getElementById('goButton'),
-      droneDevice = null,
-      gattServer = null;
-
 
   function discover() {
     console.log('Searching for drone...');
@@ -136,6 +153,7 @@
 
   }
 
+  /*
   function handshake() {
 
     console.log('Handshake...');
@@ -150,7 +168,7 @@
 
             console.log('Characteristic', characteristic);
 
-            const handshakeCmd = new Uint8Array([0x04, 0x01, 0x00, 0x04, 0x01, 0x00, 0x32, 0x30, 0x31, 0x34, 0x2D, 0x31, 0x30, 0x2D, 0x32, 0x38, 0x00]);
+            const handshakeCmd = new Uint8Array([0x04, ++steps.fa0b, 0x00, 0x04, 0x01, 0x00, 0x32, 0x30, 0x31, 0x34, 0x2D, 0x31, 0x30, 0x2D, 0x32, 0x38, 0x00]);
 
             console.log('Writing handshake command');
 
@@ -162,6 +180,79 @@
 
   }
 
+  function startPing() {
+
+    return new Promise(function(resolve) {
+
+      ping = setInterval(() => {
+        var buffer = new Buffer(19);
+        buffer.fill(0);
+        buffer.writeInt16LE(2, 0);
+        buffer.writeInt16LE(++steps.fa0a, 1);
+        buffer.writeInt16LE(2, 2);
+        buffer.writeInt16LE(0, 3);
+        buffer.writeInt16LE(2, 4);
+        buffer.writeInt16LE(0, 5);
+        buffer.writeInt16LE((driveStepsRemaining ? 1 : 0), 6);
+
+        buffer.writeInt16LE(speeds.roll, 7);
+        buffer.writeInt16LE(speeds.pitch, 8);
+        buffer.writeInt16LE(speeds.yaw, 9);
+        buffer.writeInt16LE(speeds.altitude, 10);
+        buffer.writeFloatLE(0, 11);
+
+        return gattServer.getPrimaryService(SERVICES.fa00)
+          .then(service => {
+            return service.getCharacteristic(WRITE_WITHOUT_RESPONSE_CHARACTERISTICS.fa0a)
+          })
+          .then(characteristic => {
+            console.log('Ping write');
+            characteristic.writeValue(buffer);
+          });
+
+        if (driveStepsRemaining < 0) {
+          // go on the last command blindly
+        } else if (driveStepsRemaining > 1) {
+          // decrement the drive chain
+          driveStepsRemaining--;
+        } else {
+          // reset to hover states
+          hover();
+        }
+
+      }, 50);
+
+      setInterval(() => {
+        resolve();
+      }, 100);
+
+    });
+
+  }
+
+  function calibrate() {
+
+    return gattServer.getPrimaryService(SERVICES.fa00)
+      .then(service => {
+        return service.getCharacteristic(WRITE_WITHOUT_RESPONSE_CHARACTERISTICS.fa0b)
+      })
+      .then(characteristic => {
+        characteristic.writeValue(new Uint8Array([0x02, ++steps.fa0b & 0xFF, 0x02, 0x00, 0x00, 0x00]));
+      });
+
+  }
+  */
+
+  function hover() {
+
+    driveStepsRemaining = 0;
+    speeds.roll = 0;
+    speeds.pitch = 0;
+    speeds.yaw = 0;
+    speeds.altitude = 0;
+
+  }
+
   function takeOff() {
 
     console.log('Take off...');
@@ -170,11 +261,52 @@
       .then(service => { return service.getCharacteristic( WRITE_WITHOUT_RESPONSE_CHARACTERISTICS.fa0b ) })
       .then(characteristic => {
 
-        const takeOffCmd = new Uint8Array([0x02, 0x01, 0x00, 0x01, 0x00]);
-
         console.log('Writing take off command');
 
-        return characteristic.writeValue(takeOffCmd);
+        var buffer = new ArrayBuffer(6);
+        var array = new Uint8Array(buffer);
+        array.set([4, steps.fa0b++, 2, 0, 1, 0]);
+        characteristic.writeValue(buffer).then(function onResolve(){
+          console.log('takeoff success');
+        }, function onReject(){
+          console.log('takeoff failed');
+        });
+
+      });
+
+  }
+
+  function wait(millis) {
+
+    return new Promise(function(resolve) {
+
+      setInterval(() => {
+        resolve();
+      }, millis);
+
+    });
+
+  }
+
+  function land() {
+
+    console.log('Land...');
+
+    return gattServer.getPrimaryService( SERVICES.fa00 )
+      .then(service => { return service.getCharacteristic( WRITE_WITHOUT_RESPONSE_CHARACTERISTICS.fa0b ) })
+      .then(characteristic => {
+
+        console.log('Writing land command');
+
+        // 4, (byte)mSettingsCounter, 2, 0, 3, 0
+        var buffer = new ArrayBuffer(6);
+        var array = new Uint8Array(buffer);
+        array.set([4, steps.fa0b++, 2, 0, 3, 0]);
+        characteristic.writeValue(buffer).then(function onResolve() {
+          console.log('landing success');
+        }, function onReject() {
+          console.log('landing failed');
+        });
 
       });
 
@@ -185,8 +317,10 @@
     discover()
       .then(device => { return connect(device) })
       .then(server => { return registerNotifications(server) })
-      .then(() => { return handshake() })
+      .then(() => { return wait(1000) })
       .then(() => { return takeOff() })
+      .then(() => { return wait(5000) })
+      .then(() => { return land() })
       .catch(error => { console.error('Error', error) });
 
   }
